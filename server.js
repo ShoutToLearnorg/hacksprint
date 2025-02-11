77246cb3eb67584e2e2d97529f0fef2a5049634e
Ashish Srivastava <98154274+ShoutToLearnorg@users.noreply.github.com>
2025-02-11 08:31:16 +0530

    Initial commit

--- server.js
+++ server.js
@@ -16,31 +16,31 @@ app.get('/', (req, res) => {
 
 // Route for the Future events page
 app.get('/theFuture', (req, res) => {
-    res.render('theFuture/thefuture'); // This serves views/theFuture/thefuture.ejs
+    res.render('theFuture/thefuture');
 });
 
 app.get('/prehistoricEra', (req, res) => {
-    res.render('prehistoricEra/prehistoricEra'); // This serves views/theFuture/thefuture.ejs
+    res.render('prehistoricEra/prehistoricEra');
 });
 
 app.get('/medievalTimes', (req, res) => {
-    res.render('medievalTimes/medievalTimes'); // This serves views/theFuture/thefuture.ejs
+    res.render('medievalTimes/medievalTimes');
 });
 
 app.get('/formRegistration', (req, res) => {
-    res.render('formRegistration/formRegistration'); // This serves views/formRegistration.ejs
+    res.render('formRegistration/formRegistration');
 });
 
 app.get('/eventLocation', (req, res) => {
-    res.render('eventLocation/eventLocation'); // This serves views/formRegistration.ejs
+    res.render('eventLocation/eventLocation');
 });
 
 app.get('/gallery', (req, res) => {
-    res.render('gallery/gallery'); // This serves views/formRegistration.ejs
+    res.render('gallery/gallery');
 });
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running at https://hacksprint-production.up.railway.app/`);
 });
